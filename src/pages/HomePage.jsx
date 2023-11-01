e4a33bf73ee443360e27d789cffcce338c28b2f3
Danigee <daniav9924@gmail.com>
2023-10-31 20:46:30 -0500

    fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ export const HomePage = () => {
   return (
     <section className='mt-3'>
 
-    <h2>HomePage</h2>
+    <h2>please click on the links above to start watching the different heroes</h2>
     <hr />
 
     </section>
